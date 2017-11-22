244539f9303a4d3b49bf1acd0d6b9b989bb98f16
yiyuhao <uestcyyh@sina.com>
2017-11-22 21:32:50 +0800

    docker

--- docker/mysql/start.sh
+++ docker/mysql/start.sh
@@ -6,6 +6,6 @@ docker run -d --name mysql-sanhui \
            -v $(pwd)/data:/var/lib/mysql \
            -e MYSQL_ROOT_PASSWORD="huawei@123" \
            -e MYSQL_DATABASE="sanhui" \
-           -p 3307:3306 \
+           -P \
        mysql:5.7.19 \
            --character-set-server=utf8 --collation-server=utf8_general_ci
