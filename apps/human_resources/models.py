3e30e6dfd99eb45ea186c29953171930a23710e7
_GL0oMY <uestcyyh@sina.com>
2017-12-08 11:52:24 +0800

    修改: 人力资源model 所有可选字段加入可能最高频使用的默认值

--- apps/human_resources/models.py
+++ apps/human_resources/models.py
@@ -59,9 +59,9 @@ class PersonnelInformation(models.Model):
     permanent_residence = models.ForeignKey(Province, verbose_name='常住地')
     home = models.ForeignKey(Family, verbose_name='所在户(户主)', null=True, blank=True)
 
-    degree_of_education = models.CharField(verbose_name='文化程度', max_length=5, choices=(
+    degree_of_education = models.CharField(verbose_name='文化程度', max_length=5, default='cz', choices=(
         ('cz', '初中及以下'), ('gz', '高中'), ('dz', '大专'), ('bk', '本科'), ('ss', '硕士'), ('bs', '博士')))
-    political_status = models.CharField(verbose_name='政治面貌', max_length=6, choices=(
+    political_status = models.CharField(verbose_name='政治面貌', max_length=6, default='qz', choices=(
         ('qz', '群众'), ('ty', '团员'), ('rdjjfz', '入党积极分子'), ('ybdy', '预备党员'), ('zsdy', '正式党员')))
     school_major_field = models.CharField(verbose_name='在读院校和专业', max_length=50, null=True, blank=True)
     employment_intention = models.ForeignKey(EmploymentIntention, verbose_name='就业意向', null=True, blank=True)
@@ -74,10 +74,10 @@ class PersonnelInformation(models.Model):
 
     working_place = models.CharField(verbose_name='务工地点', max_length=30, null=True, blank=True)
     working_years = models.CharField(verbose_name='务工年限', max_length=4, null=True, blank=True, choices=(
-        ('1', '1年以下'), ('1to5', '1-5年'), ('5', '5年以上')))
+        ('1', '1年以下'), ('1to5', '1-5年'), ('5', '5年以上')), default='1to5')
     working_industry = models.CharField(verbose_name='务工行业', max_length=30, null=True, blank=True)
     working_salary = models.CharField(verbose_name='务工年收入', max_length=5, null=True, blank=True, choices=(
-        ('5', '5万以下'), ('5to8', '5-8万'), ('8to12', '8-12万'), ('12', '12万以上')))
+        ('5', '5万以下'), ('5to8', '5-8万'), ('8to12', '8-12万'), ('12', '12万以上')), default='5')
     working_position = models.CharField(verbose_name='务工职位', max_length=30, null=True, blank=True)
 
     phone_num = models.CharField(verbose_name='联系方式', max_length=11, null=True, blank=True)
